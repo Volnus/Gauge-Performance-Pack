538fe62990436728492bf0f7f050434acc044945
Volnus <scotth0407@gmail.com>
2016-03-06 12:17:57 -0600

    Update performancepack.php
    Tons of useless lines have been removed the code is now updated to work even if you don't have certain plugins enabled.. Conditional loading is now taken to a whole new level To where some pages will have 10 less styles and scripts maybe even more!

--- performancepack.php
+++ performancepack.php
@@ -3,10 +3,15 @@
  * Plugin Name:       Gauge Performance Pack
  * Plugin URI:        https://github.com/Volnus/Gauge-Performance-Pack/
  * Description:       This plugin makes several modifications to the WordPress admin and the Frontend to improve the experience and performance of the website.
- * Version:           1.0
+ * Version:           2.0
  * Author:            Scott Hartley
  * Author URI:        http://thearcadecorner.com
 */
+/*****************************************
+* 1. Reset
+* 2. Conditional Loading 
+*****************************************/
+/******* 1. Reset ********/
 
 // Remove query string from static files
 function remove_cssjs_ver( $src ) {
@@ -17,53 +22,107 @@ function remove_cssjs_ver( $src ) {
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
 
-
-// Solve Auto Optimize Cache Problems
-add_filter('autoptimize_js_include_inline','gad81_ao_include_inline',10,1);
-function gad81_ao_include_inline() {
-        return false;
-	}
-
-// Fix Write A Review Page
-
-add_filter('autoptimize_filter_noptimize','my_ao_noptimize',10,0);
-function my_ao_noptimize() {
-	if (strpos($_SERVER['REQUEST_URI'],'/write-a-review/')!==false) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-// Fix Change Avatar Page
-add_filter('autoptimize_filter_noptimize','second_ao_noptimize',10,0);
-function second_ao_noptimize() {
-if (strpos($_SERVER['REQUEST_URI'],'/change-avatar/')!==false) {
-return true;
-} else {
-return false;
-}
-
-// Turn Off Admin Bar
-//add_filter('show_admin_bar', '__return_false');
-
-// Dequeue the Emoji script
-//function disable_emoji_dequeue_script() { wp_dequeue_script( 'emoji' ); } add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
-
-// Remove the emoji styles
-//remove_action( 'wp_print_styles', 'print_emoji_styles' ); remove_action( 'wp_print_scripts', 'print_emoji_detection_script' ); remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); remove_action( 'admin_print_styles', 'print_emoji_styles'); remove_action( 'admin_print_scripts','print_emoji_detection_script');
-
 // Hide Generator Tags
-//remove_action('wp_head', 'wp_generator'); remove_action('wp_head', 'woocommerce_generator'); add_action('init', 'myoverride', 100);function myoverride() {remove_action('wp_head', array(visual_composer(), 'addMetaData'));}
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'woocommerce_generator');
+
+//Turn Off Admin Bar
+add_filter('show_admin_bar', '__return_false');
+
+/******* 2. CONDITIONAL LOADING ********/
+
+//Dequeue the Emoji script
+function disable_emoji_dequeue_script() { wp_dequeue_script( 'emoji' ); } add_action( 'wp_print_scripts', 'disable_emoji_dequeue_script', 100 );
+
+//Remove the emoji styles
+remove_action( 'wp_print_styles', 'print_emoji_styles' ); remove_action( 'wp_print_scripts', 'print_emoji_detection_script' ); remove_action( 'wp_head', 'print_emoji_detection_script', 7 ); remove_action( 'admin_print_styles', 'print_emoji_styles'); remove_action( 'admin_print_scripts','print_emoji_detection_script');
+
 
 // Optimize WooCommerce
-//add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 ); function child_manage_woocommerce_styles() { first check that woo exists to prevent fatal errors if ( function_exists( 'is_woocommerce' ) ) { //dequeue scripts and styles if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) { wp_dequeue_style( 'woocommerce-layout' ); wp_dequeue_style( 'woocommerce-smallscreen' ); wp_dequeue_style( 'woocommerce-general' ); wp_dequeue_script( 'wc-add-to-cart' ); wp_dequeue_script( 'wc-cart-fragments' ); wp_dequeue_script( 'woocommerce' ); wp_dequeue_script( 'jquery-blockui' ); wp_dequeue_script( 'jquery-placeholder' ); } } }
+add_action( 'wp_enqueue_scripts', 'child_manage_woocommerce_styles', 99 );
+ 
+function child_manage_woocommerce_styles() {
+ 
+	//first check that woo exists to prevent fatal errors
+	if ( function_exists( 'is_woocommerce' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_woocommerce() && ! is_cart() && ! is_checkout() ) {
+wp_dequeue_style( 'woocommerce-layout' );
+wp_dequeue_style( 'woocommerce-smallscreen' );
+wp_dequeue_style( 'woocommerce-general' );
+wp_dequeue_style( 'gp-woocommerce' );  
+wp_dequeue_script( 'wc-add-to-cart' );
+wp_dequeue_script( 'wc-cart-fragments' );
+wp_dequeue_script( 'woocommerce' );
+wp_dequeue_script( 'jquery-blockui' );
+wp_dequeue_script( 'jquery-placeholder' );
+		}
+	}
+ 
+}
 
-// Dequeue bbPress CSS and .js on non-forum pages.
-//function isa_dequeue_bbp_style() { if ( class_exists('bbPress') ) { if ( ! is_bbpress() ) { wp_dequeue_style('bbp-default'); wp_dequeue_style( 'bbp_private_replies_style'); wp_dequeue_script('bbpress-editor'); } } } add_action( 'wp_enqueue_scripts', 'isa_dequeue_bbp_style', 99 );
 
-// Deregister Contact Form 7 styles
-//add_action( 'wp_print_styles', 'aa_deregister_styles', 100 ); function aa_deregister_styles() { if ( ! is_page( 'contact-us' ) ) { wp_deregister_style( 'contact-form-7' ); } }
+// Optimize BuddyPress
+add_action( 'wp_enqueue_scripts', 'conditional_buddypress_styles_scripts', 99 );
+ 
+function conditional_buddypress_styles_scripts() {
+ 
+	//first check that buddypress exists to prevent fatal errors
+	if ( function_exists( 'is_buddypress' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_buddypress() ) {
+    wp_dequeue_style('bp-mentions-css');
+    wp_dequeue_style('gp-bp');
+    wp_dequeue_style('bp-legacy-css');
+    wp_dequeue_script('bp-confirm');
+    wp_dequeue_script('bp-widget-members');
+    wp_dequeue_script('bp-jquery-query');
+    wp_dequeue_script('bp-jquery-cookie');
+    wp_dequeue_script('bp-jquery-scroll-to');
+    wp_dequeue_script('bp-legacy-js');
+    wp_dequeue_script('jquery-atwho');
+    wp_dequeue_script('bp-mentions');
+		}
+	}
+ 
+}
 
-// Deregister Contact Form 7 JavaScript files on all pages without a form
-//add_action( 'wp_print_scripts', 'aa_deregister_javascript', 100 ); function aa_deregister_javascript() { if ( ! is_page( 'contact-us' ) ) { wp_deregister_script( 'contact-form-7' ); } }
+
+// Optimize bbPress
+add_action( 'wp_enqueue_scripts', 'conditional_bbpress_styles_scripts', 99 );
+ 
+function conditional_bbpress_styles_scripts() {
+ 
+	//first check that bbpress exists to prevent fatal errors
+	if ( function_exists( 'is_bbpress' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_bbpress() ) {
+        wp_dequeue_style('bbp-default-css');
+        wp_dequeue_style('gp-bbp');
+        wp_dequeue_style('bbp-default');
+        wp_dequeue_style( 'bbp_private_replies_style');
+        wp_dequeue_script('bbpress-editor');
+		}
+	}
+}
+
+// Optimize Posts
+add_action( 'wp_enqueue_scripts', 'conditional_post_styles_scripts', 99 );
+ 
+function conditional_post_styles_scripts() {
+ 
+	//first check that single posts exists to prevent fatal errors
+	if ( function_exists( 'is_single' ) ) {
+		//dequeue scripts and styles
+		if ( ! is_single() ) {
+        wp_dequeue_style('post-views-counter-frontend');
+        wp_dequeue_style('wp-mediaelement');
+        wp_dequeue_style('mediaelement');
+        wp_dequeue_script('post-views-counter-frontend');
+        wp_dequeue_script('wp-mediaelement');
+        wp_dequeue_script('mediaelement');
+        wp_dequeue_script('wp-embed');
+		}
+	}
+ 
+}
